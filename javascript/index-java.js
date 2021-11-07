8ba8a648a6fc7a1082a550afc846c5ecf86f751a
Emilie johnsen <eaaebjoh@students.eaaa.dk>
2021-11-07 17:47:22 +0100

    Emilies version

--- javascript/index-java.js
+++ javascript/index-java.js
@@ -20,13 +20,56 @@ observer.observe(video);
 // Javascript for progressbar
 
 const side2 = document.getElementById("side2");
-const jordbaer = document.getElementById("2")
+const jordbaer = document.getElementById("2");
+const side3 = document.getElementById("side3");
+const peach = document.getElementById("3");
+const bob2 = document.getElementById("bob2");
+const bob3 = document.getElementById("bob3");
 
 side2.addEventListener("mouseover", function(){
     jordbaer.classList.add("scale")
+    bob2.classList.add("shake-top")
 
 })
 
 side2.addEventListener("mouseout", function(){
     jordbaer.classList.remove("scale")
 })
+
+side3.addEventListener("mouseover", function(){
+    peach.classList.add("scale")
+    bob3.classList.add("shake-bottom")
+})
+
+side3.addEventListener("mouseout", function(){
+    peach.classList.remove("scale")
+})
+
+// Javascript for side 3 "Vælg en ret" 
+
+const gris = document.getElementById("gris");
+const fisk = document.getElementById("fisk");
+const ko = document.getElementById("ko");
+const kylling = document.getElementById("kylling");
+const vegetar = document.getElementById("vegetar");
+
+gris.addEventListener("click", function(){
+    const output = document.getElementById("output");
+    output.innerHTML = "Godt valg! <br> Så laver vi stegt flæsk med persillesovs!";
+    gris.classList.add("valg");
+    fisk.classList.remove("valg");
+    ko.classList.remove("valg");
+    kylling.classList.remove("valg");
+    vegetar.classList.remove("valg");
+});
+
+fisk.addEventListener("click", function(){
+    const output = document.getElementById("output");
+    output.innerHTML = `Godt valg! <br> Så laver vi laks med salat og pasta!`;
+    gris.classList.remove("valg");
+    fisk.classList.add("valg");
+    ko.classList.remove("valg");
+    kylling.classList.remove("valg");
+    vegetar.classList.remove("valg");
+});
+
